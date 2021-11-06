bfd46fa3fb8751ab232af2cd93938b6d1cdf5d1f
Sergey Korshunov <s.korshunov@dev.zencode.team>
2021-11-06 11:23:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Screen from "./Screen";
+import { useState } from "react";
+import GameControl from "./GameControl";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [isGameRunning, setIsGameRunning] = useState(false)
+
+    let isGameRunning = false;
+    const fieldSize = [50, 35];
+
+    const initArr = (x, y) => {
+        return [...Array(x)].map((_, i) => [...Array(y)].map((_, j) =>
+            ({ x: i, y: j, isFilled: ( i === 0 || i === x - 1 || j === 0 || j === y - 1 ) })));
+    };
+
+    const [presentScreenState, setPresentScreenState] = useState(initArr(...fieldSize))
+
+    const applyChangesToScreen = changes => {
+        setPresentScreenState(previousArrState => previousArrState
+            .map(row => row.map(rowItem => changes.map(change => (change.x === rowItem.x && change.y ===rowItem.y) ?
+                { ...rowItem, isFilled: change.isFilled } :
+                rowItem
+            )))
+        )
+    };
+
+    const getNextDirection = () => {
+
+    }
+    const nextAction = () => {
+        setTimeout(() => {
+            applyChangesToScreen([{x:5, y: 5, isFilled:true }])
+            console.log('wow!')
+        }, 500)
+
+        isGameRunning && nextAction()
+    };
+
+    const startGame = () => {
+        // setIsGameRunning(true)
+        isGameRunning = true
+        nextAction()
+
+    }
+
+    const stopGame = () => {
+        // setIsGameRunning(false);
+        isGameRunning = false;
+        // setPresentScreenState(initArr(...fieldSize))
+    }
+
+    return (
+        <div className={styles.App}>
+            <Screen
+                presentScreenState={presentScreenState}
+            />
+            <div className={styles.buttonPanel}>
+                <button className={styles.button} onClick={startGame}>Start</button>
+                <button className={styles.button} onClick={stopGame}>Stop</button>
+            </div>
+            <GameControl getNextDirection={getNextDirection} />
+        </div>
+    );
 }
 
 export default App;
